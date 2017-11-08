9b407ff8827da36be49c6c819b68264d07872a20
Per Bolmstedt <per@kodexe.se>
2017-11-08 20:30:37 +0100

    initial commit

---
+++ vs2017/iPMC.HtmlTemplate.Intellisense/iPMC.HtmlTemplate.Intellisense/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("iPMC.HtmlTemplate.Intellisense")]
+[assembly: AssemblyDescription("Enable VS intellisense & type safety for iPMC HTML templates")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("inRiver Community")]
+[assembly: AssemblyProduct("iPMC.HtmlTemplate.Intellisense")]
+[assembly: AssemblyCopyright("inRiver Community")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df6af483-aaa4-4cc7-8ba2-2388e09d685d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ vs2017/iPMC.HtmlTemplate.Intellisense/iPMC.HtmlTemplate.Intellisense/inRiver/TemplateHelper.cs
@@ -0,0 +1,111 @@
+﻿using inRiver.Remoting.Objects;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace iPMC.HtmlTemplate.Intellisense.inRiver
+{
+
+    /// <summary>
+    /// From https://community.inriver.com/product/technical-descriptions/edit-in-context-templates/ as of 2017-11-08
+    /// </summary>
+    public class TemplateHelper
+    {
+
+        /// <summary>Outputs display name.</summary>
+        /// <example>@TemplateHelper.DisplayName(Model)</example>
+        public static IHtmlString DisplayName(Entity entityModel) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs display description.</summary>
+        /// <example>@TemplateHelper.DisplayDescription(Model)</example>
+        public static IHtmlString DisplayDescription(Entity entityModel) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs the url for the Entity default picture, the size parameter defaults to "preview"</summary>
+        /// <example><div><img src="@TemplateHelper.PictureUrl(subModel)" style="max-width: 100px;" /></div></example>
+        public static IHtmlString PictureUrl(Entity entityModel, string size = null) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a field value for a given Entity system id.</summary>
+        /// <example>@TemplateHelper.FieldValue(1234, "ArtNumber")</example>
+        public static IHtmlString FieldValue(int entityId, string fieldTypeId) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a field value for a given Entity.</summary>
+        /// <example>@TemplateHelper.FieldValue(Model, "ArtNumber")</example>
+        public static IHtmlString FieldValue(Entity entityModel, string fieldTypeId) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs localized field type name.</summary>
+        /// <example>@TemplateHelper.FieldTypeName("ArtNumber")</example>
+        public static IHtmlString FieldTypeName(string fieldTypeId) { throw new NotImplementedException(); }
+
+        /// <summary>For Edit Templates only. Outputs an edit icon. When clicked, the full Entity editor is shown.</summary>
+        /// <example>@TemplateHelper.EntityEditor(1234)</example>
+        public static IHtmlString EntityEditor(int entityId) { throw new NotImplementedException(); }
+
+        /// <summary>For Edit Templates only. Outputs an edit icon. When clicked, the full Entity editor is shown.</summary>
+        /// <example>@TemplateHelper.EntityEditor(Model)</example>
+        public static IHtmlString EntityEditor(Entity entityModel) { throw new NotImplementedException(); }
+
+        /// <summary>For Edit Templates only. Outputs an edit icon. When clicked, the a single field editor is shown.</summary>
+        /// <example>@TemplateHelper.FieldEditor(1234, "assortmentName")</example>
+        public static IHtmlString FieldEditor(int entityId, string fieldTypeId) { throw new NotImplementedException(); }
+
+        /// <summary>For Edit Templates only. Outputs an edit icon. When clicked, the a single field editor is shown.</summary>
+        /// <example>@TemplateHelper.FieldEditor(Model, "assortmentName")</example>
+        public static IHtmlString FieldEditor(Entity entityModel, string fieldTypeId) { throw new NotImplementedException(); }
+
+        /// <summary>Returns an array of outbound linked Entity system IDs.</summary>
+        /// <example>@foreach(var subEntityId in TemplateHelper.GetSubEntityIds(1234, "Product"))
+        ///     {
+        /// <li>@subEntityId</li>
+        ///     }
+        /// }
+        /// </example>
+        public static IEnumerable<IHtmlString> GetSubEntityIds(int entityId, string entityType = null) { throw new NotImplementedException(); }
+
+        /// <summary>Returns an array of outbound linked Entities.</summary>
+        /// <example>@foreach (var subModel in TemplateHelper.GetSubEntityModels(Model, "Product"))
+        /// {
+        /// <li>
+        /// @TemplateHelper.FieldValue(subModel, "ArtNumber")
+        /// @TemplateHelper.FieldEditor(subModel, "ArtNumber")
+        /// </li>
+        /// }</example>
+        public static IEnumerable<Entity> GetSubEntityModels(Entity entityModel, string entityType = null) { throw new NotImplementedException(); }
+
+        /// <summary>Returns an array of outbound linked Entities.</summary>
+        /// <example>@foreach (var subModel in TemplateHelper.GetSubEntityModels(1234, "Product"))
+        /// {
+        /// <li>
+        /// @TemplateHelper.FieldValue(subModel, "ArtNumber") 
+        /// @TemplateHelper.FieldEditor(subModel, "ArtNumber")
+        /// </li>
+        /// }</example>
+        public static IEnumerable<Entity> GetSubEntityModels(int entityId, string entityType = null) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a javascript object containing all values of a CVL.</summary>
+        /// <example><script type="text/javascript">
+        /// var compositionTypes = @TemplateHelper.RenderCvlValuesJson("CompositionType")
+        /// var compositionMaterials = @TemplateHelper.RenderCvlValuesJson("CompositionMaterial")
+        /// </script></example>
+        public static IHtmlString RenderCvlValuesJson(string cvlId) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a javascript object containing all Entity data and outbound linked Entities.</summary>
+        /// <example><script type="text/javascript">
+        /// var data = @TemplateHelper.RenderFullEntityJson(Model.Id)
+        /// </script></example>
+        public static IHtmlString RenderFullEntityJson(int entityId) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a javascript object containing basic Entity information.</summary>
+        /// <example><script type="text/javascript">
+        /// var basicData = @TemplateHelper.RenderBasicEntityJson(Model.Id)
+        /// </script></example>
+        public static IHtmlString RenderBasicEntityJson(int entityId) { throw new NotImplementedException(); }
+
+        /// <summary>Outputs a javascript array containing basic Entity information.</summary>
+        /// <example><script type="text/javascript">
+        /// var basicDatas = @TemplateHelper.RenderLinkedBasicEntityJson(Model.Id)
+        /// </script></example>
+        public static IHtmlString RenderLinkedBasicEntityJson(int entityId) { throw new NotImplementedException(); }
+
+    }
+
+}
